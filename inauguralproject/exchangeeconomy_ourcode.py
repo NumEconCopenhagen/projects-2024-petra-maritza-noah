364ead59d7e0a4fea2f6513632cba2831f620867
PetraCentis <tzr932@alumni.ku.dk>
2024-03-13 18:45:42 +0100

    question 3

--- inauguralproject/exchangeeconomy_ourcode.py
+++ inauguralproject/exchangeeconomy_ourcode.py
@@ -14,6 +14,14 @@ class ExchangeEconomyClass:
         par.w1A = 0.8
         par.w2A = 0.3
 
+        #ADDING PARAMETERS THAT I AM NOT REALLY SURE:
+        #par.N = N # number of agents
+        par.k = 2 # relative endowment of good 1
+        par.kappa = 0.1 # Adjustment factor for solving
+        par.eps = 1e-8 # Tolerance parameter for solving
+        par.maxiter=500 # Max iterations when solving
+
+
     def utility_A(self,x1A,x2A):
         par = self.par
         return x1A**par.alpha * x2A**(1 - par.alpha)
@@ -47,30 +55,46 @@ class ExchangeEconomyClass:
         return eps1,eps2
     
 #TRYING TO DO SOMETHING:
-    def check_market_clearing1(self,p1):
+    #def check_market_clearing1(self,p1):
+    def check_market_clearing1(self,p1, p2):
+
+        par = self.par
+
+        x1A,x2A = self.demand_A(p1, p2)
+        x1B,x2B = self.demand_B(p1, p2)
+
+        epss1 = x1A-par.w1A + x1B-(1-par.w1A)
+
+        return epss1
+    
+    def check_market_clearing2(self,p1):
 
         par = self.par
 
         x1A,x2A = self.demand_A(p1)
         x1B,x2B = self.demand_B(p1)
 
-        epss1 = x1A-par.w1A + x1B-(1-par.w1A)
+        epss2 = x2A-par.w2A + x2B-(1-par.w2A)
 
-        return epss1
+        return epss2
 
   #STILL HAVE TO FIX THIS CODE:
+    
+    #def find_equilibrium(self,p1_guess,p2):
       
-    def find_equilibrium(self,p1_guess,p2):
-        
+    def find_equilibrium(self,p1_guess,p2, N, k, eps, kappa, maxiter):
+        import numpy as np
         t = 0
         p1 = p1_guess
+        epss1 = self.check_market_clearing1(p1, p2)
         
         # using a while loop as we don't know number of iterations a priori
         while True:
 
             # a. step 1: excess demand
-            Z1 = epss1(p1,p2)
-            
+            epss1 = self.check_market_clearing1(p1, p2)
+            Z1 = epss1
+
             # b: step 2: stop?
             if  np.abs(Z1) < eps or t >= maxiter:
                 print(f'{t:3d}: p1 = {p1:12.8f} -> excess demand -> {Z1:14.8f}')
@@ -83,24 +107,24 @@ class ExchangeEconomyClass:
                 print('   ...')
             
             # d. step 3: update p1
-            p1 = p1 + self.kappa*Z1/self.N
+            p1 = p1 + kappa*Z1/N
             
             # e. step 4: update counter and return to step 1
             t += 1    
 
 
         # Check if solution is found 
-        if np.abs(Z1) < self.eps:
+        if np.abs(Z1) < eps:
             # Store equilibrium prices
             self.p1_star = p1 
             self.p2_star = p2
 
             # Store equilibrium excess demand 
             self.Z1 = Z1
-            self.Z2 = self.excess_demand_good_2_func(self.p1_star, self.p2_star)
+            self.Z2 = self.check_market_clearing1(self.p1_star, self.p2_star)
 
             # Make sure that Walras' law is satisfied
-            if not np.abs(self.Z2)<self.eps:
+            if not np.abs(self.Z2)< eps:
                 print('The market for good 2 was not cleared')
                 print(f'Z2 = {self.Z2}')
 
