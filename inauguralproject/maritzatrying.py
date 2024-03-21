51c85481c345a982e3554906b5b3aa78c4c460f4
maritzahuang <kmb785@alumni.ku.dk>
2024-03-21 10:33:27 +0100

    new changes Ex8

--- inauguralproject/maritzatrying.py
+++ inauguralproject/maritzatrying.py
@@ -222,14 +222,13 @@ class ExchangeEconomyClass:
         text += f'Z2 = {self.Z2}'
         print(text)
 
-    
-
     #QUESTION 6A:
     
     def utility_aggregate(self,x1A,x2A):
         par = self.par
         return x1A**par.alpha * x2A**(1 - par.alpha) + (1-x1A)**par.beta * (1-x2A)**(1 - par.beta)
     
+    #QUESTION 8:
   
 
 
