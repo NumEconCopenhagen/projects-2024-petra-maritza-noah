5f96f1e78ad08b7e432b2b5abc017d174360db5f
PetraCentis <tzr932@alumni.ku.dk>
2024-05-04 11:52:03 +0200

    change

--- modelproject/Petra.py
+++ modelproject/Petra.py
@@ -73,85 +73,9 @@ class BeckerTomesModelClass:
 
         return self.sol
 
-# # Usage
+ # Usage for the notebook
 # model = BeckerTomesModelClass()
 # solution = model.solve_model()
-# print(f'Optimal parental investment: {solution.Xt_1_optimal}')
+# print(f'Optimal parental investment: {solution.X0_optimal}')
 # print(f'Human capital level: {solution.H1_optimal}')
 # print(f'Optimal income: {solution.Y1_optimal}')
-
-
-
-
-
-
-    # def solve_discrete(self,do_print=False):
-    #     """ solve model discretely """
-        
-    #     par = self.par
-    #     sol = self.sol
-    #     opt = SimpleNamespace()
-        
-    #     # a. all possible choices
-    #     x = np.linspace(0,1,49)
-    #     S0,S1 = np.meshgrid(x,x) # all combinations
-    
-    #     S0 = S0.ravel() # vector
-    #     S1 = S1.ravel()
-
-    #     # b. calculate utility
-    #     v = self.calc_utility(S0,S1)
-    
-    #     # c. set to minus infinity if constraint is broken
-    #     I = (S0 > 1) | (S1 > 1) # | is "or"
-    #     v[I] = -np.inf
-    
-    #     # d. find maximizing argument
-    #     j = np.argmax(v)
-        
-    #     opt.S0 = S0[j]
-    #     opt.S1 = S1[j]
-    #     opt.v = v[j]
-
-    #     # e. print
-    #     if do_print:
-    #         for k,v in opt.__dict__.items():
-    #             print(f'{k} = {v:6.4f}')
-
-    #     return opt
-    
-    # def solve_continously(self,do_print=False):
-    #     """ solve model continously """
-
-    #     par = self.par
-    #     sol = self.sol
-    #     opt = SimpleNamespace()
-
-    #     # a. calculate utility with negative since we will use minimize()
-    #     def u(x):
-    #          return -self.calc_utility(x[0],x[1])
-
-    #     # b. constraints and bounds
-    #     bounds = optimize.Bounds([0, 0],[1, 1])
-    #     #linear_constraint = optimize.LinearConstraint([[ 0, 0], [0, 0, 1, 1]], [0, 0], [25, 25])
-
-    #     # c. initial guess
-    #     x_guess = np.array([0.5,0.5])
-
-    #     # d. find maximization
-    #     ans = optimize.minimize(u, x_guess, method="SLSQP", bounds=bounds)
-
-    #     opt.S0 = ans.x[0]
-    #     opt.S1 = ans.x[1]
-    #     opt.v = -ans.fun
-
-    #     # e. print answer
-    #     if do_print:
-    #         for k,v in opt.__dict__.items():
-    #             print(f'{k} = {v:6.4f}')
-    #     # print(ans.message)
-    #     # print(f'LM = {ans.x[0]:.0f}, HM = {ans.x[1]:.0f}, LF = {ans.x[2]:.0f}, HF = {ans.x[3]:.0f}, Utility = {ans.fun:.4f}')
-    #     return opt
-    
-
-
